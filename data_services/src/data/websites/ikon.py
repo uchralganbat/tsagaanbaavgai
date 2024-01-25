74941e5ee08ec66e8bbdb7c2a402a0bf477b0c8e
uchralganbat <uchralganbat0220@gmail.com>
2024-01-25 23:07:48 +0800

    refactoring data pipelines

--- data_services/src/data/websites/ikon.py
+++ data_services/src/data/websites/ikon.py
@@ -1,4 +1,4 @@
-from crawler.base import Base
+from websites.base import Base
 import pandas as pd
 
 
@@ -14,6 +14,10 @@ class Ikon(Base):
             "title": [],
         }
 
+        # self.df = pd.DataFrame(
+        #     columns=["date", "headline", "url", "main", "author", "title"]
+        # )
+
         self.category = {
             "politics": "l/1",
             "economics": "l/2",
@@ -25,14 +29,11 @@ class Ikon(Base):
         pages = soup.find_all("div", {"class": "ikp_item"})
 
         for page in pages:
-            print(page.get("data-url"))
             page_soup = self.request_and_parse(page.get("data-url"))
 
             items = page_soup.find_all("div", {"class": "nlitem"})
             for item in items:
                 self.extract_item(item)
-
-            print(self.data)
 
         return pd.DataFrame(self.data, columns=self.data.keys())
 
@@ -40,28 +41,36 @@ class Ikon(Base):
         try:
             article_url = item.find("a").get("href")
             match article_url:
-                case "opinion":
+                case "opinion" if "opinion" in article_url:
                     print("opinion article")
                     return
+
                 case _:
                     date = item.find("div", {"class": "nldate"}).get("rawdate")
-
                     headline = item.find("div", {"class": "nlheadline"}).text.strip()
-
                     title = item.find("div", {"class": "nlheader"}).text.strip()
 
                     soup = self.request_and_parse(article_url)
                     news = soup.find("div", {"class": "inews"})
 
                     main = " ".join([p.text for p in news.find_all("p")])
-
                     author = news.find("div", {"class": "name"}).text
+
+                    # row = {
+                    #     "date": date,
+                    #     "headline": headline,
+                    #     "url": f"{self.base_url}/{article_url}",
+                    #     "main": main,
+                    #     "author": author,
+                    #     "title": title,
+                    # }
 
         except Exception as e:
             print(e)
             print(article_url)
             return
 
+        # self.df = self.df.append(row, ignore_index=True)
         self.data["date"].append(date)
         self.data["headline"].append(headline)
         self.data["url"].append(f"{self.base_url}/{article_url}")
